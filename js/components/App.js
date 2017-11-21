2558843e5ff0ae3cd1662126d966882d96554538
dfarina <dfarina@idom.com>
2017-11-21 17:54:49 +0100

    Cambios

--- js/components/App.js
+++ js/components/App.js
@@ -1,37 +1,60 @@
+var fps, fpsInterval, startTime, now, then, elapsed;
+
 var App = createReactClass({
 
     getInitialState() {
         return {
             screen: { width: this.props.playgroundSize.w, height: this.props.playgroundSize.h },
             context: null,
-            elements: this.props.elements
+            elements: []
         }
     },
 
     componentDidMount() {
         var context = this.refs.canvas.getContext('2d');
-        this.setState({ context: context });
+        this.setState({
+            context: context,
+            elements: {
+                "Ants": [new Ant(this.state)]
+            }
+        });
+
+        fpsInterval = 1000 / 60;
+        then = performance.now();
+        startTime = then;
+
         requestAnimationFrame(() => { this.update() });
     },
 
     update() {
-        this.state.context.save();
-        // context.scale(this.state.screen.ratio, this.state.screen.ratio);
 
-        // Motion trail
-        this.state.context.fillStyle = '#FFF';
-        this.state.context.globalAlpha = 0.4;
-        this.state.context.fillRect(0, 0, this.state.screen.width, this.state.screen.height);
-        this.state.context.globalAlpha = 1;
+        now = performance.now();
+        elapsed = now - then;
 
-        // Remove or render
-        this.updateObjects(this.state.elements, 'Ants')
+        
+
+        if (elapsed > fpsInterval) {
 
 
-        this.state.context.restore();
+            this.state.context.save();
+            // context.scale(this.state.screen.ratio, this.state.screen.ratio);
 
-        // Next frame
+            // Motion trail
+            this.state.context.fillStyle = '#FFF';
+            this.state.context.globalAlpha = 0.4;
+            this.state.context.fillRect(0, 0, this.state.screen.width, this.state.screen.height);
+            this.state.context.globalAlpha = 1;
+
+            // Remove or render
+            this.updateObjects(this.state.elements, 'Ants')
+
+
+            this.state.context.restore();
+            then = now - (elapsed % fpsInterval);
+            
+        }
         requestAnimationFrame(() => { this.update() });
+
     },
 
     updateObjects(items, group) {
@@ -59,6 +82,5 @@ var App = createReactClass({
 })
 
 App.defaultProps = {
-    playgroundSize: { h: 800, w: 1000 },
-    elements: { "Ants": [new Ant()] }
+    playgroundSize: { h: 800, w: 1000 }
 }
--- js/models/Ant.js
+++ js/models/Ant.js
@@ -1,41 +1,57 @@
-var Ant = function () {
+var Ant = function (worldState) {
     this.location = { x: 10, y: 10 };
     this.orientation = { x: 0, y: 0 };
     this.speed = 1;
-    this.fieldView = 3;
+    this.fieldView = 10;
     this.type = Ant.static.types[1];
     this.destination = { x: 100, y: 100 };
     this.varAbs = 0;
 
-    this.setDestination = function () {
-        this.destination.x = Math.floor((Math.random() * (100 + this.location.x)) + this.location.x);
-        this.destination.y = Math.floor((Math.random() * (100 + this.location.y)) + this.location.y);
+    this.setDestination = function (state) {
+        this.destination.x = this.location.x + Math.floor((Math.random() * (200) - 100));
+        this.destination.y = this.location.y + Math.floor((Math.random() * (200) - 100));
+
+        this.destination.x = this.destination.x < 0 ? Math.floor(-this.destination.x) : Math.floor(this.destination.x);
+        this.destination.y = this.destination.y < 0 ? Math.floor(-this.destination.y) : Math.floor(this.destination.y);
+
+        this.destination.x = this.destination.x > state.screen.width ? state.screen.width : Math.floor(this.destination.x);
+        this.destination.y = this.destination.y > state.screen.height ? state.screen.height : Math.floor(this.destination.y);
+
         this.varAbs = Math.sqrt(Math.pow(this.destination.x - this.location.x, 2) + Math.pow(this.destination.y - this.location.y, 2));
         this.orientation.x = (this.destination.x - this.location.x) / this.varAbs;
         this.orientation.y = (this.destination.y - this.location.y) / this.varAbs;
-        console.log("new loc x:" + this.destination.x + " Y:" + + this.destination.y);
+        console.log("new dest x:" + this.destination.x + " Y:" + this.destination.y + " Angulo "+ (Math.atan2(this.orientation.x,this.orientation.y)*(180/Math.PI)));
     }
 
-    this.move = function () {
-        if (Math.floor(this.location.x) == this.destination.x && Math.floor(this.location.y) == this.destination.y) {
+    this.move = function (state) {
+        if (Math.abs(this.destination.x - this.location.x) < this.fieldView && Math.abs(this.destination.y - this.location.y) < this.fieldView) {
             this.orientation = { x: 0, y: 0 };
-            this.setDestination();
+            this.setDestination(state);
         }
         this.location.x = this.location.x + this.orientation.x * this.speed;
         this.location.y = this.location.y + this.orientation.y * this.speed;
     }
 
     this.render = function (state) {
-        this.move();
+        this.move(state);
         var ctx = state.context;
         ctx.save()
+        ctx.rotate(Math.atan2(this.orientation.x,this.orientation.y)*(180/Math.PI));
         ctx.beginPath();
+        ctx.strokeRect(this.location.x,this.location.y,4,4);
         ctx.fillStyle = '#FFF';
-        ctx.arc(this.location.x, this.location.y, 1, 0, 2 * Math.PI);
+        //ctx.arc(this.location.x, this.location.y, 1, 0, 2 * Math.PI);
         ctx.stroke();
         ctx.restore();
+
+        ctx.save()
+        ctx.beginPath();
+        ctx.fillStyle = '#FF0000';
+        ctx.arc(this.destination.x, this.destination.y, 1, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.restore();
     }
-    this.setDestination();
+    this.setDestination(worldState);
 }
 
 Ant.static = {
